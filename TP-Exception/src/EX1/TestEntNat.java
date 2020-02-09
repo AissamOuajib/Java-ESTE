ddf16a8fef676be82352a52fb977316a0ec80b03
OuajibDev <ouajibdev@gmail.com>
2020-02-09 22:43:29 +0100

    Fourth Commit

--- TP-Exception/src/EX1/TestEntNat.java
+++ TP-Exception/src/EX1/TestEntNat.java
@@ -3,9 +3,9 @@ package EX1;
 public class TestEntNat {
 	public static void main(String[] args) {
 		try {
-			EntNat entNat1 = new EntNat(10);
+			EntNat entNat1 = new EntNat(11);
 			System.out.println("entNat1 : " + entNat1.getN());
-			EntNat entNat2 = new EntNat(-7);
+			EntNat entNat2 = new EntNat(-9);
 			System.out.println("entNat2 : " + entNat2.getN());
 
 		} catch (ErrConst e) {
--- TP-Exception/src/EX2/TestEntNat.java
+++ TP-Exception/src/EX2/TestEntNat.java
@@ -3,20 +3,20 @@ package EX2;
 public class TestEntNat {
 	public static void main(String[] args) {
 		try {
-			System.out.println("la somme :" + EntNat.somme(1, 89));
-			System.out.println(EntNat.somme(Integer.MAX_VALUE , 20));
+			System.out.println("la somme :" + EntNat.somme(10, 9));
+			System.out.println(EntNat.somme(Integer.MAX_VALUE , 12));
 
 		} catch (ErrSom e) {
 			System.out.println(e.getMessage());
 			try {
-				System.out.println("Difference :" + EntNat.difference(19, 11));
-				System.out.println(EntNat.difference(Integer.MAX_VALUE +2, 1));
+				System.out.println("Difference :" + EntNat.difference(9, 3));
+				System.out.println(EntNat.difference(Integer.MAX_VALUE +4, 1));
 
 			} catch (ErrDiff e1) {
 				System.out.println(e1.getMessage());
 				try {
-					System.out.println("produit :" + EntNat.produit(40, 10));
-					System.out.println(EntNat.produit(Integer.MAX_VALUE +1, 9));
+					System.out.println("produit :" + EntNat.produit(5, 1));
+					System.out.println(EntNat.produit(Integer.MAX_VALUE +3, 19));
 				} catch (ErrProd e2) {
 					System.out.println(e2.getMessage());
 				}
